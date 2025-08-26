958958b780903acc9d69d8d155cbbc7865ed065e
kimym98 <tampler48@gmail.com>
2025-08-26 10:03:49 +0900

    chore : build에러 임시수정

--- src/pages/Login/components/LoginForm.tsx
+++ src/pages/Login/components/LoginForm.tsx
@@ -6,8 +6,14 @@ function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(email);
+    alert(password);
+  };
+
   return (
-    <form className="flex flex-col gap-2">
+    <form className="flex flex-col gap-2" onSubmit={handleLogin}>
       <div className="flex flex-col gap-5 pt-8">
         <EmailField onChange={(email: string) => setEmail(email)} />
         <PasswordField onChange={(email: string) => setPassword(email)} />
