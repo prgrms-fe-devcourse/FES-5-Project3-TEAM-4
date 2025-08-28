ccf03b8ce74857d5c4983f6b0cc90a9afe95eb88
cheezstick <yooha922@gmail.com>
2025-08-28 10:24:04 +0900

    style : 타로 셔플 기본 ui 구성

---
+++ src/pages/TarotShuffle/index.tsx
@@ -0,0 +1,13 @@
+import ShuffleCards from './components/ShuffleCards';
+import StopShuffleBtn from './components/StopShuffleBtn';
+
+function TarotShuffle() {
+  return (
+    <div className="flex flex-col py-16 w-full h-[90vh] justify-center items-center gap-10 text-main-white">
+      <ShuffleCards></ShuffleCards>
+      <h2>질문을 마음 속으로 충분히 그려보신 뒤 카드 뽑기를 시작하세요.</h2>
+      <StopShuffleBtn></StopShuffleBtn>
+    </div>
+  );
+}
+export default TarotShuffle;
