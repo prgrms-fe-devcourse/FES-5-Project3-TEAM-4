7076e750eb62cfea22a2e588cf61e84db45864b0
cheezstick <yooha922@gmail.com>
2025-08-29 15:27:59 +0900

    feat : 타로 셔플 ~ 스프레드 연결

--- src/pages/Tarot/index.tsx
+++ src/pages/Tarot/index.tsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import TarotQuestion from '../TarotQuestion';
 import TarotShuffle from '../TarotShuffle';
+import TarotSpreadDraw from '../TarotSpreadDraw';
 import { type TopicLabel } from '@/common/types/TarotTopics';
 
 export default function Tarot() {
-  const [stage, setStage] = useState<'question' | 'shuffle'>('question');
+  const [stage, setStage] = useState<'question' | 'shuffle' | 'spread'>('question');
   const [question, setQuestion] = useState('');
   const [topic, setTopic] = useState<TopicLabel | null>(null);
 
   return (
     <div className="bg-gray-800">
-      {stage === 'question' ? (
+      {stage === 'question' && (
         <TarotQuestion
           presetQuestion={question}
           selectedTopicInitial={topic}
@@ -20,9 +21,11 @@ export default function Tarot() {
             setStage('shuffle');
           }}
         />
-      ) : (
-        <TarotShuffle />
       )}
+
+      {stage === 'shuffle' && <TarotShuffle onProceed={() => setStage('spread')} />}
+
+      {stage === 'spread' && <TarotSpreadDraw />}
     </div>
   );
 }
--- src/pages/TarotShuffle/index.tsx
+++ src/pages/TarotShuffle/index.tsx
@@ -1,12 +1,16 @@
 import ShuffleCards from './components/ShuffleCards';
 import StopShuffleBtn from './components/StopShuffleBtn';
 
-export default function TarotShuffle() {
+type Props = {
+  onProceed: () => void;
+};
+
+export default function TarotShuffle({ onProceed }: Props) {
   return (
     <div className="flex flex-col py-16 w-full h-[90vh] justify-center items-center gap-10 text-main-white">
       <ShuffleCards />
       <h2>질문을 마음 속으로 충분히 그려보신 뒤 카드 뽑기를 시작하세요.</h2>
-      <StopShuffleBtn />
+      <StopShuffleBtn onClick={onProceed} />
     </div>
   );
 }
