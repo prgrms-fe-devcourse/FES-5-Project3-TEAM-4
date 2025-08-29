4b03c78f94e9e529a0058054efefcdf26659fece
kimym98 <tampler48@gmail.com>
2025-08-29 12:17:53 +0900

    Feat : 마이페이지 Post, LIke 데이터 fetch

--- src/pages/Mypage/components/Record.tsx
+++ src/pages/Mypage/components/Record.tsx
@@ -5,16 +5,20 @@ function Record() {
   return (
     <section className=" w-[750px] h-[85vh] flex flex-col gap-5 pt-10">
       <h1 className="text-main-white pt-14 text-2xl font-semibold">Record</h1>
-      <div className="w-[750px] h-[800px] relative">
-        <div className="w-[100%] h-[800px]  rounded-[20px] p-4 absolute z-30">
-          <img src="/icons/boy3.svg" className="absolute" alt="달에서 낚시하는 소년 이미지" />
-          <div className="flex flex-col gap-20">
+      <div className="w-[750px] h-[90%] relative">
+        <div className="w-[100%] h-[95%] rounded-[20px] p-4 absolute z-30">
+          <div className="top-0 flex flex-col h-[100%] items-center pt-4  absolute">
+            <img src="/icons/boy2.svg" alt="달에서 낚시하는 소년 이미지" />
+            <div className="w-px ml-[98px] flex-1 bg-white/25" />
+            <img className="ml-[98px]" src="/icons/stargroup.svg" alt="" />
+          </div>
+          <div className="flex flex-col gap-18">
             <div className="ml-[152px]">
-              <p className="text-main-white text-base font-semibold pt-8">
+              <p className="text-main-white text-base font-semibold pt-8 ">
                 간직하고 싶은 기억을 기록하고 언제든 다시 확인해보세요
               </p>
             </div>
-            <ul className="pt-4 overflow-auto max-h-[450px] scrollbar-thin scrollbar-thumb-amber-200 scrollbar-track-transparent">
+            <ul className="pt-4 overflow-auto h-[40vh] scrollbar-thin scrollbar-thumb-amber-200 scrollbar-track-transparent">
               <RecordItem />
             </ul>
           </div>
--- src/pages/Mypage/components/RecordItem.tsx
+++ src/pages/Mypage/components/RecordItem.tsx
@@ -1,8 +1,8 @@
 function RecordItem() {
   return (
-    <li className="mb-8">
-      <a className="flex gap-9.5 items-center" href="#">
-        <span className="text-main-white text-xs">2025-08-22</span>
+    <li className="flex items-center mb-3 rounded-3xl h-[32px] bg-white/5 shadow-[0_4px_50px_5px_rgba(0,0,0,0.20)] backdrop-blur-[30px]">
+      <a className="flex gap-6.5 items-center" href="#">
+        <span className="text-main-white text-xs ml-6">2025-08-22</span>
         <img src="/icons/timelineBtn.svg" alt="" />
         <span className="text-main-white text-xs">기록해보세요</span>
       </a>
