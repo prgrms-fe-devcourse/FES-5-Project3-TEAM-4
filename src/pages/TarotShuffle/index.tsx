ad99edb80d06e7d29fcc0c6784fb7787e23662f6
taeeun98 <xodms6360@naver.com>
2025-09-04 14:44:24 +0900

    style : 벨벳 어두운 버전 추가

--- src/pages/TarotShuffle/index.tsx
+++ src/pages/TarotShuffle/index.tsx
@@ -21,14 +21,14 @@ export default function TarotShuffle({ onProceed }: Props) {
       style={{
         backgroundImage: "url('/velvet.png')",
         backgroundRepeat: 'no-repeat',
-        backgroundSize: '100% 100%', // 강제로 div 크기에 맞춤
+        backgroundSize: '100% 100%',
       }}
     >
       <div className="flex flex-col items-center gap-10 flex-grow justify-center">
         <ShuffleCards />
       </div>
 
-      <div className="flex flex-col gap-5 mb-10">
+      <div className="flex flex-col gap-5 mb-10 pl-4 pr-4">
         <h2>질문을 마음속으로 충분히 그려보신 뒤, 멈추고 싶을 때 멈추시면 됩니다.</h2>
 
         <StopShuffleBtn onClick={goNext} />
